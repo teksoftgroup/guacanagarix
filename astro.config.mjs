c18a37ca773148fe9cc8025baacd15bdba78aa11
Pascal Anglade <pascal.nglade@gmail.com>
2025-10-20 19:58:15 -0400

    Fix astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://teksoftgroup.github.io/guacanagarix/",
+  base: "/guacanagarix/",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
